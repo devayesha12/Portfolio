f6ae06bef57d0d8aa46d6b61ffb182311b4ee536
Ezequote <ezequote.billing@gmail.com>
2026-01-07 21:09:14 +0500

    Fix build errors: resolve missing PDF, remove unused imports, fix accessibility warnings

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,8 +4,6 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import Techstack from "../About/Techstack";
-import Github from "../About/Github";
 import {
   AiFillGithub,
   AiOutlineTwitter,
@@ -71,7 +69,7 @@ function Home() {
               </li>
               <li className="social-icons">
                 <a
-                  href=""
+                  href="https://twitter.com/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
